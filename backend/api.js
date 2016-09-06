846c54080d4cbceebf6274b6c968c8c06dd31734
Cat <catherinemwood93@gmail.com>
2016-09-05 18:06:10 -0700

    server created and running on local host 3000 saying hi

--- backend/api.js
+++ backend/api.js
@@ -1,2 +1,14 @@
 var express = require("express");
 console.log(express);
+
+var app = express();
+var router = express.Router();
+router.get('/', function(req, res) {
+    res.send('../app/index.html');
+});
+
+app.use('/', router);
+app.listen(3000, function() {
+    console.log("Server listening on port %d", 3000);
+
+});
